0f036bf8fdb5a4eb75d577755fb56d75f0b98ad0
JulienCote <julien.jc.cote@gmail.com>
2015-12-04 01:36:14 -0500

    day 4 is done

---
+++ day4/advent_miner.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2.7
+
+import hashlib
+
+def validate_key(key):
+	if (key[:6] == '000000'):
+		return True
+	return False
+
+hasher = hashlib.md5()
+secret_key = 'bgvyzdsv'
+answer = 1
+
+hasher.update(secret_key + str(answer))
+
+while not validate_key(hasher.hexdigest()):
+	hasher = hashlib.md5()
+	answer += 1
+	hasher.update(secret_key + str(answer))
+
+print answer
+print hasher.hexdigest()
+
+
+
+
+
+
