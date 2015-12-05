a3b334e4b873211f323257e081c1be9b4f9bd8da
JulienCote <julien.jc.cote@gmail.com>
2015-12-05 05:15:52 -0500

    Dia cinco!!!

---
+++ day5/naughty_tester.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2.7
+
+nice_string_counter = 0
+
+with open('input.txt') as fp:
+
+	for line in fp:
+		previous_char = ''
+		vowel_count = 0
+		double_char = False
+		evil_chars = False
+
+		for char in line:
+			last_2_char = previous_char + char
+			print last_2_char
+			if ((last_2_char == 'ab') or (last_2_char == 'cd') or (last_2_char == 'pq') or (last_2_char == 'xy')):
+				evil_chars = True
+			if(char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u'):
+				vowel_count += 1
+			if char == previous_char:
+				double_char = True
+
+			previous_char = char
+
+		if (double_char and vowel_count >= 3 and not evil_chars):
+			nice_string_counter += 1
+		
+
+	print nice_string_counter
+
+
+
---
+++ day5/naughty_tester_part2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2.7
+
+nice_string_counter = 0
+
+with open('input.txt') as fp:
+
+	for line in fp:
+		very_previous_char = ''
+		previous_char = ''
+
+		pairing_time = 0
+		letter_pair = False
+		inbetween_repeater = False
+		char_pair_record = []
+
+		for char in line:
+			if ((previous_char + char) in char_pair_record[:-1]):
+				letter_pair = True
+
+			char_pair_record.append(previous_char + char)
+
+			if (very_previous_char == char):
+				inbetween_repeater = True
+
+
+			very_previous_char = previous_char
+			previous_char = char
+			
+		if (letter_pair and inbetween_repeater):
+			nice_string_counter += 1
+		
+
+	print nice_string_counter
+
+
+
