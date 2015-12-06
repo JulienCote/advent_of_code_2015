4d110f26c16651129c221b8df388106b58f6e7ea
JulienCote <julien.jc.cote@gmail.com>
2015-12-06 16:45:23 -0500

    day 6

---
+++ day6/light_switch_part2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2.7
+
+import re
+import numpy
+
+light_matrix = numpy.zeros((1000,1000), dtype= numpy.int)
+regex = '^([a-z ]+)(\d+)(?:,)(\d+)(?: \w+ )(\d+)(?:,)(\d+)'
+
+with open('input.txt') as fp:
+
+	for line in fp:
+		instructions = re.search(regex, line)
+
+		for x_axis in range(int(instructions.group(2)), int(instructions.group(4))+1):
+			for y_axis in range(int(instructions.group(3)), int(instructions.group(5))+1):
+
+				if instructions.group(1) == 'turn on ':
+					light_matrix[x_axis, y_axis] += 1
+				elif instructions.group(1) == 'turn off ' and light_matrix[x_axis, y_axis] > 0:
+					light_matrix[x_axis, y_axis] -= 1
+				elif instructions.group(1) == 'toggle ':
+					light_matrix[x_axis, y_axis] += 2
+
+print numpy.sum(light_matrix)
